803adcfc645a85ccca77c90492ad3407d237d5a8
Toan Nguyen <mtoanhcm@gmail.com>
2024-07-14 01:37:27 +0700

    update test logic to create map

---
+++ Assets/Scripts/Map/MapGroundManager.cs
@@ -0,0 +1,39 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGroundManager : MonoBehaviour
+{
+    [SerializeField]
+    private Grid mapGrid;
+    [SerializeField]
+    private Tilemap groundMap;
+    [SerializeField]
+    private TileBase groundTile; // Assign the tile in the inspector
+
+    [SerializeField]
+    private int radius;
+
+    [Button]
+    private void CreateTileMap()
+    {
+        // Set the size and tiles
+        groundMap.ClearAllTiles();
+        CreateTilemap(radius); // Example size of 10x10
+    }
+
+    private void CreateTilemap(int radius) {
+        for (int x = -radius; x <= radius; x++)
+        {
+            for (int y = -radius; y <= radius; y++)
+            {
+                SetTile(x, y);
+            }
+        }
+    }
+
+    private void SetTile(int x, int y) {
+        Vector3Int tilePosition = new (x, y, 0);
+        groundMap.SetTile(tilePosition, groundTile);
+    }
+}
